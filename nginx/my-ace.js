20a6071bb6f2971bdfa06ba63577ac71da0ae1c4
Laurent Carrie <laurent.carrie@gmail.com>
2025-02-13 10:48:33 +0100

    x

--- nginx/my-ace.js
+++ nginx/my-ace.js
@@ -3,5 +3,6 @@ function my_edit(id) {
     var editor = ace.edit(id);
     editor.setTheme("ace/theme/twilight");
     editor.session.setMode("ace/mode/javascript");
+    console.log("set value") ;
     editor.setValue("the new text here");
 }
