ab90e6b83b2c2338526d07be64bf3a3a1417af14
Laurent Carrie <laurent.carrie@gmail.com>
2025-02-13 11:44:44 +0100

    x

--- software/leptos/source_tree/src/lib.rs
+++ software/leptos/source_tree/src/lib.rs
@@ -145,6 +145,7 @@ yyy
                                         <button on:click=move |_| {
                                             log!("edit") ;
                                             let data= fs::read_to_string(mjf.as_str()).unwrap();
+                                            log!("{}",&data) ;
                                             let aedit=my_edit("editor",&data) ;
                                             // let aedit = my_edit("editor") ;
                                             // editor.setTheme("ace/theme/twilight");
