1e20b83238a5c866dfd5f35801f8d30c34ac6964
Laurent Carrie <laurent.carrie@gmail.com>
2025-02-17 13:28:15 +0100

    x

--- software/leptos/source_tree/src/lib.rs
+++ software/leptos/source_tree/src/lib.rs
@@ -234,7 +234,7 @@ extern "C" {
 }
 
 #[component]
-pub fn EditFile(url: String, id: String) -> impl IntoView {
+pub fn EditFile(url: String, editor_id: String) -> impl IntoView {
     let file_data = AsyncDerived::new_unsync(move || fetch_file(url.clone()));
 
     let fallback = move |errors: ArcRwSignal<Errors>| {
