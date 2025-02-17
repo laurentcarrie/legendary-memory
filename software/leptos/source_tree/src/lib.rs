31a096edf8635e7d8ec2f531874baa9b7954eca1
Laurent Carrie <laurent.carrie@gmail.com>
2025-02-17 15:54:51 +0100

    x

--- software/leptos/source_tree/src/lib.rs
+++ software/leptos/source_tree/src/lib.rs
@@ -313,7 +313,7 @@ pub fn EditFile(label: String, url: String, editor_id: String) -> impl IntoView 
                                         }>edit</button>
                                         <button
                                         on:click=move |_| {
-                                             let data=my_get_data(&editor) ;
+                                             let data=my_get_data(&editor2) ;
                                              log!("{}",&data) ;
                                                     ()
                                         }>save</button>
