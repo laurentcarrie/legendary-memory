20c9192e6e02eec7ca4d2677101141f8286661b9
Laurent Carrie <laurent.carrie@gmail.com>
2025-02-17 15:50:26 +0100

    x

--- software/leptos/source_tree/src/lib.rs
+++ software/leptos/source_tree/src/lib.rs
@@ -304,14 +304,19 @@ pub fn EditFile(label: String, url: String, editor_id: String) -> impl IntoView 
                                     // let editor=my_edit(id.as_str(),"hello world",10) ;
                                     let editor=my_edit("editor","hello world",10) ;
                                     view! {
-                                                <button
-                                        // prop:name=move || label
-                                        prop:name=move || "xxx"
+                                        <button
                                         on:click=move |_| {
-                                     let nblines = text.chars().filter(|c| *c == '\n').count();
-                                     my_set_data(&editor,&text,nblines) ;
-                                            ()
+                                             let nblines = text.chars().filter(|c| *c == '\n').count();
+                                             my_set_data(&editor,&text,nblines) ;
+                                                    ()
                                         }>edit</button>
+                                        <button
+                                        on:click=move |_| {
+                                             let nblines = text.chars().filter(|c| *c == '\n').count();
+                                             data=my_get_data(&editor) ;
+                                             log!(&data) ;
+                                                    ()
+                                        }>save</button>
 
                                     }
                                     })}
