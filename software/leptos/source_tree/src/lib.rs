8b0f04ce910b96c89c91e81afe78667a46252db1
Laurent Carrie <laurent.carrie@gmail.com>
2025-02-18 14:09:08 +0100

    x

--- software/leptos/source_tree/src/lib.rs
+++ software/leptos/source_tree/src/lib.rs
@@ -179,7 +179,7 @@ yyy
                                                 let index=c.0 ;
                                                 let c=c.1 ;
                                                 view! { <option value={
-                                                    let data = BASE64_STANDARD.encode(c.clone() ) ;
+                                                    let data = BASE64_STANDARD.encode(serde_json::to_str(c.clone()).unwrap() ) ;
                                                     data
                                                     }>{c.author.clone()} @ {c.title.clone()}</option>}
                                             }).collect::<Vec<_>>()
