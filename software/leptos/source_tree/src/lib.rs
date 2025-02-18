6dd43b499bd6082fd88de22571eef9772a036e72
Laurent Carrie <laurent.carrie@gmail.com>
2025-02-18 11:49:04 +0100

    x

--- software/leptos/source_tree/src/lib.rs
+++ software/leptos/source_tree/src/lib.rs
@@ -139,7 +139,7 @@ yyy
                                 <label>songs</label>
                                 <div id="songpick-id">
                                         <label for="cars">Choose a car:</label>
-                                        <select name="cars" id="cars">
+                                        <Select name="cars" id="cars">
                                           <option value="volvo">Volvo</option>
                                           <option value="saab">Saab</option>
                                           <option value="mercedes">Mercedes</option>
