884a5fb63c9dac6c0f8a7c615c3447aa9086d361
Laurent Carrie <laurent.carrie@gmail.com>
2025-02-18 16:31:02 +0100

    x

--- software/leptos/source_tree/src/lib.rs
+++ software/leptos/source_tree/src/lib.rs
@@ -227,6 +227,18 @@ yyy
                                         </li>
                                         </ul>
                                     }
+                                    view! {
+                                        <ul>
+                                        {move || { c.texfiles.into_iter().map(|f|{
+                                            view! {
+                                                <li>
+                                                    <EditFile label="master json".to_string() url=f.clone() editor_id="editor".to_string() />
+                                                </li>
+                                                }
+                                            }).collect::<Vec<_>>() ;
+                                            }}
+                                        </ul>
+                                    }
                                 }}
                                             // let data = BASE64_STANDARD.decode(value.get()).expect("valid base64 string");
 
