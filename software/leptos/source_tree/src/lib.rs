1a9b070f191951fe62a2ddb8ed90553e27cb6f77
Laurent Carrie <laurent.carrie@gmail.com>
2025-02-18 14:03:10 +0100

    x

--- software/leptos/source_tree/src/lib.rs
+++ software/leptos/source_tree/src/lib.rs
@@ -189,7 +189,7 @@ yyy
 
                                 <div>
                                     <label>{value}</label><br/>
-                                    <label>{
+                                    <label>move || {
                                             log!("label value : {}",value.read()) ;
                                             let data = BASE64_STANDARD.decode(value.get()).expect("valid base64 string");
                                             let data = String::from_utf8(data).expect("utf8 string");
