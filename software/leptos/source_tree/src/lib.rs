57da5274ff37b56cdf79f2e50f23a6d65575259a
Laurent Carrie <laurent.carrie@gmail.com>
2025-02-18 18:14:30 +0100

    x

--- software/leptos/source_tree/src/lib.rs
+++ software/leptos/source_tree/src/lib.rs
@@ -279,53 +279,53 @@ yyy
 
 
 
-                                // {move || {
-                                //     log!("{}",value.get()) ;
-                                //     let c = base64_to_item(value.get()) ;
-                                //     view! {
-                                //         <ul>
-                                //         json master file
-                                //         <li>
-                                //             <EditFile label="master json".to_string() url=c.masterjsonfile.clone() mode="ace/mode/json".to_string() editor_id="editor".to_string() />
-                                //         </li>
-                                //         </ul>
-                                //     }
-                                // }}
-                                //
-                                //
-                                // {move || {
-                                //     log!("{}",value.get()) ;
-                                //     let c = base64_to_item(value.get()) ;
-                                //     view! {
-                                //         <ul> tex files
-                                //         { c.texfiles.clone().into_iter().map(|f| {
-                                //             view!{
-                                //         <li>
-                                //             <EditFile label="master json".to_string() url=f.clone() mode="ace/mode/latex".to_string() editor_id="editor".to_string() />
-                                //         </li>
-                                //                 }
-                                //             }).collect::<Vec<_>>()
-                                //             }
-                                //         </ul>
-                                //     }
-                                // }}
+                                {move || {
+                                    log!("{}",value.get()) ;
+                                    let c = base64_to_item(value.get()) ;
+                                    view! {
+                                        <ul>
+                                        json master file
+                                        <li>
+                                            <EditFile label="master json".to_string() url=c.masterjsonfile.clone() mode="ace/mode/json".to_string() editor_id="editor".to_string() />
+                                        </li>
+                                        </ul>
+                                    }
+                                }}
 
-                                // {move || {
-                                //     log!("{}",value.get()) ;
-                                //     let c = base64_to_item(value.get()) ;
-                                //     view! {
-                                //         <ul> tex files for lyrics sections
-                                //         { c.lyricstexfiles.clone().into_iter().map(|f| {
-                                //             view!{
-                                //         <li>
-                                //             <EditFile label="master json".to_string() url=f.clone() mode="ace/mode/latex".to_string() editor_id="editor".to_string() />
-                                //         </li>
-                                //                 }
-                                //             }).collect::<Vec<_>>()
-                                //             }
-                                //         </ul>
-                                //     }
-                                // }}
+
+                                {move || {
+                                    log!("{}",value.get()) ;
+                                    let c = base64_to_item(value.get()) ;
+                                    view! {
+                                        <ul> tex files
+                                        { c.texfiles.clone().into_iter().map(|f| {
+                                            view!{
+                                        <li>
+                                            <EditFile label="master json".to_string() url=f.clone() mode="ace/mode/latex".to_string() editor_id="editor".to_string() />
+                                        </li>
+                                                }
+                                            }).collect::<Vec<_>>()
+                                            }
+                                        </ul>
+                                    }
+                                }}
+
+                                {move || {
+                                    log!("{}",value.get()) ;
+                                    let c = base64_to_item(value.get()) ;
+                                    view! {
+                                        <ul> tex files for lyrics sections
+                                        { c.lyricstexfiles.clone().into_iter().map(|f| {
+                                            view!{
+                                        <li>
+                                            <EditFile label="master json".to_string() url=f.clone() mode="ace/mode/latex".to_string() editor_id="editor".to_string() />
+                                        </li>
+                                                }
+                                            }).collect::<Vec<_>>()
+                                            }
+                                        </ul>
+                                    }
+                                }}
 
                                 {move || {
                                     log!("{}",value.get()) ;
