5fac0245cefd2286c1d25e095dd8a8fb7c7383b9
Laurent Carrie <laurent.carrie@gmail.com>
2025-02-19 15:51:24 +0100

    x

--- software/leptos/source_tree/src/lib.rs
+++ software/leptos/source_tree/src/lib.rs
@@ -130,7 +130,7 @@ pub fn App() -> impl IntoView {
             .map(|value| {
                 log!("YYYYYY") ;
                 // format!("Server returned {value:?}")
-                value.to_string()
+                format!("{}",value)
             })
             // This loading state will only show before the first load
             .unwrap_or_else(|| "XXXXX Loading...".into())
