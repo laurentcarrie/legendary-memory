b823a5acc405645ac28d394efe39ff5496087fe3
Laurent Carrie <laurent.carrie@gmail.com>
2025-02-19 17:36:56 +0100

    x

--- software/leptos/source_tree/src/lib.rs
+++ software/leptos/source_tree/src/lib.rs
@@ -17,29 +17,9 @@ pub mod protocol;
 use protocol::model::answer::{Choice, EChoice, SourceTree,SourceTreeItem};
 use protocol::model::request as request ;
 
-fn default_world() -> SourceTree {
-    SourceTree { items: vec![] }
-}
+pub mod util ;
+use util::base64_to_item ;
 
-fn base64_to_item(input:String) -> SourceTreeItem {
-    let data : Option<Vec<u8>> = BASE64_STANDARD.decode(input.as_str()).ok() ;
-    let data : Option<String> = data.map(|c| String::from_utf8(c).ok()).flatten() ;
-    let c:Option<SourceTreeItem> = data.map(|s| serde_json::from_str(s.as_str()).ok()).flatten();
-    match c {
-        Some(c) => c,
-        None => {
-            log!("could not parse input string to SourceTreeItem : {}",&input) ;
-            SourceTreeItem{
-                title:"error".to_string(),
-                author:"error".to_string(),
-                masterjsonfile: "error".to_string(),
-                texfiles:vec![],
-                lyricstexfiles:vec![],
-                lyfiles:vec![]
-            }
-        }
-    }
-}
 
 async fn fetch_world() -> Result<SourceTree> {
     gloo_timers::future::TimeoutFuture::new(1000).await;
---
+++ software/leptos/source_tree/src/util.rs
@@ -0,0 +1,28 @@
+use base64::prelude::BASE64_STANDARD;
+use base64::prelude::*;
+
+use protocol::model::answer::{Choice, EChoice, SourceTree,SourceTreeItem};
+
+fn default_world() -> SourceTree {
+    SourceTree { items: vec![] }
+}
+
+fn base64_to_item(input:String) -> SourceTreeItem {
+    let data : Option<Vec<u8>> = BASE64_STANDARD.decode(input.as_str()).ok() ;
+    let data : Option<String> = data.map(|c| String::from_utf8(c).ok()).flatten() ;
+    let c:Option<SourceTreeItem> = data.map(|s| serde_json::from_str(s.as_str()).ok()).flatten();
+    match c {
+        Some(c) => c,
+        None => {
+            log!("could not parse input string to SourceTreeItem : {}",&input) ;
+            SourceTreeItem{
+                title:"error".to_string(),
+                author:"error".to_string(),
+                masterjsonfile: "error".to_string(),
+                texfiles:vec![],
+                lyricstexfiles:vec![],
+                lyfiles:vec![]
+            }
+        }
+    }
+}
