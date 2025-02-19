0e3fb47542c2cc5b4ba8730736aae4242230dea2
Laurent Carrie <laurent.carrie@gmail.com>
2025-02-19 13:52:48 +0100

    x

--- software/leptos/source_tree/src/lib.rs
+++ software/leptos/source_tree/src/lib.rs
@@ -137,7 +137,8 @@ pub fn App() -> impl IntoView {
                     let p = match document.get_element_by_id("xxx") {
                         Some(p) => {
                             log!("found document xxx") ;
-                            let file_data = AsyncDerived::new_unsync(move || fetch_file(&path.clone()));
+                            let p2=path.clone() ;
+                            let file_data = AsyncDerived::new_unsync(move || fetch_file(p2));
                             let editor = my_edit("editor","sss",10) ;
                             p
                         }
