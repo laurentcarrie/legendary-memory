95bfffe7800b78143dd320cc64fa33b5684f6057
Laurent Carrie <laurent.carrie@gmail.com>
2025-02-19 17:46:29 +0100

    x

--- software/leptos/source_tree/src/util.rs
+++ software/leptos/source_tree/src/util.rs
@@ -78,7 +78,7 @@ pub async fn save_file(path: String,content:String) -> Result<()> {
 
 
 // get file data from url
-async fn fetch_file(path: String) -> Result<String> {
+pub async fn fetch_file(path: String) -> Result<String> {
     log!("fetch file {}",path) ;
     gloo_timers::future::TimeoutFuture::new(1000).await;
     // make the request
