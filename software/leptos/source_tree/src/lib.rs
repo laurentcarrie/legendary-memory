ce11c38681166660ccbe126cc3ae242d36b07857
Laurent Carrie <laurent.carrie@gmail.com>
2025-02-20 17:17:53 +0100

    x

--- software/leptos/source_tree/src/lib.rs
+++ software/leptos/source_tree/src/lib.rs
@@ -52,14 +52,7 @@ pub fn App() -> impl IntoView {
     let async_file_save_data = LocalResource::new(move || save_file(file_save_value.get().0,file_save_value.get().1));
     let async_build_data = LocalResource::new(move || { let _ = build_value.get() ; build()});
     let async_omake_children_data = LocalResource::new(move || { let _ = omake_children_value.get() ; omake_children_info() });
-    let (xeditor,set_xeditor) = signal::<Vec<u8>>({
-        log!("creating editor...") ;
-        let e = my_edit("","","",10) ;
-        log!("creating editor done") ;
-        let array = Uint8Array::new(&e);
-        let bytes: Vec<u8> = array.to_vec();
-        bytes
-    }) ;
+    let (xeditor,set_xeditor) = signal::<Option<Vec<u8>>>(None) ;
 
     let async_file_result = move || {
         async_file_data
