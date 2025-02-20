8225f3d3944cfef26c163059c484c179235fd30e
Laurent Carrie <laurent.carrie@gmail.com>
2025-02-20 10:38:21 +0100

    x

--- software/leptos/source_tree/src/lib.rs
+++ software/leptos/source_tree/src/lib.rs
@@ -56,7 +56,7 @@ pub fn App() -> impl IntoView {
                     Ok(t) => {
                         let (url,t) = t ;
                         let nblines = t.chars().filter(|c| *c == '\n').count();
-                        let extension = PathBuf::from(url) ;
+                        let extension = PathBuf::from(url).extension().unwrap_or(OsStr("")) ;
                         log!("extension : {:?}",extension) ;
                         let editor = my_edit("editor","sss","ace/mode/json",nblines) ;
                         my_set_data(&editor,t.clone().as_str(),30) ;
