07518bce52098a87949d3a38385ed4e7e2deef2f
Laurent Carrie <laurent.carrie@gmail.com>
2025-02-20 11:14:28 +0100

    x

--- software/leptos/source_tree/src/lib.rs
+++ software/leptos/source_tree/src/lib.rs
@@ -61,8 +61,7 @@ pub fn App() -> impl IntoView {
                             let p = PathBuf::from(&url) ;
                             let e = &p.extension().unwrap_or(default);
                             log!("extension : {:?}", &e);
-                            e.clone().clone()
-                            // ()
+                            ()
                         };
                         let editor = my_edit("editor", "sss", "ace/mode/json", nblines);
                         my_set_data(&editor, t.clone().as_str(), 30);
