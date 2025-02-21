2899e47f485328155354ef232bef9077a5f13515
Laurent Carrie <laurent.carrie@gmail.com>
2025-02-21 13:12:18 +0100

    x

--- software/leptos/source_tree/src/util.rs
+++ software/leptos/source_tree/src/util.rs
@@ -95,7 +95,7 @@ pub async fn fetch_file(path: String) -> Result<(String, String)> {
 pub async fn build(id:String) -> Result<()> {
     let choice = request::Choice { choice:request::EChoice::ItemBuild(id) } ;
     let json = serde_json::to_string(&choice).unwrap();//   echo "{\"choice\":{\"ItemBuild\": null}}" | base64
-    let path =  format!("/scripts/request.sh?request={}",json).as_str() ;
+    let path =  format!("/scripts/request.sh?request={}",&json).as_str() ;
     log!("build, url is {}", path);
     gloo_timers::future::TimeoutFuture::new(1000).await;
     // make the request
