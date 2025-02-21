c8a0900539498789fb9370e7ca8264728d734328
Laurent Carrie <laurent.carrie@gmail.com>
2025-02-21 11:08:16 +0100

    x

--- nginx/my-ace.js
+++ nginx/my-ace.js
@@ -2,8 +2,9 @@ function my_edit(id,value,mode,nblines) {
     console.log("create edit") ;
     var editor = ace.edit(id);
     console.log("type of editor : ",typeof(editor)) ;
-    console.log(JSON.stringify(editor));
-    console.log("after stringify") ;
+    // var j = JSON.string
+    // console.log(JSON.stringify(editor));
+    // console.log("after stringify") ;
     editor.setTheme("ace/theme/twilight");
     editor.session.setMode(mode);
     // console.log("set value") ;
