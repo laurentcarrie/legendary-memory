8c85ce8a63de7f6ddccbc2a334bea23b5dd3616c
Laurent Carrie <laurent.carrie@gmail.com>
2025-02-24 17:41:53 +0100

    x

--- software/leptos/source_tree/src/lib.rs
+++ software/leptos/source_tree/src/lib.rs
@@ -341,7 +341,7 @@ edit me...
                 {
                     log!("show build progress") ;
                     set_file_value.set("".to_string()) ;
-                    set_what_to_show(WhatToShow::OmakeStdout) ;
+                    set_what_to_show.set(WhatToShow::OmakeStdout) ;
                     ()
                 }>"progress (stdout)"</button>
 
