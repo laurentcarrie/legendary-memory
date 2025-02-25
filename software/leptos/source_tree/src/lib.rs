7126616d8ae8643a0c3a2eb69990c60beaeb07e1
Laurent Carrie <laurent.carrie@gmail.com>
2025-02-25 10:15:06 +0100

    ok

--- software/leptos/source_tree/src/lib.rs
+++ software/leptos/source_tree/src/lib.rs
@@ -63,7 +63,7 @@ pub fn App() -> impl IntoView {
     let async_file_data = LocalResource::new(move || get_file(file_value.get()));
     let async_file_save_data = LocalResource::new(move || save_file(file_save_value.get().0,file_save_value.get().1));
     let async_build_data = LocalResource::new(move || { log!("xxx build") ;
-        let now = build_value.get() ; build(now)
+        let now = build_value.get() ; build(Some(now))
     });
     // let async_omake_children_data = LocalResource::new(move || { let _ = omake_children_value.get() ; omake_children_info() });
 
