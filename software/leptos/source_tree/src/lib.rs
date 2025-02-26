25764bb9078079d75c373aeef58ce3a79805bd5a
Laurent Carrie <laurent.carrie@gmail.com>
2025-02-26 15:00:24 +0100

    x

--- software/leptos/source_tree/src/lib.rs
+++ software/leptos/source_tree/src/lib.rs
@@ -222,7 +222,7 @@ edit me...
                                         set_song_value.set(ev.target().value().parse().expect("set_value"));
                                         log!("song value is {}",song_value.get()) ;
                                         let c  = SourceTreeItem_of_base64(song_value.get()) ;
-                                        set_what_to_show(WhatToShow::SourceFile(c.masterjsonfile) ;
+                                        set_what_to_show(WhatToShow::SourceFile(c.masterjsonfile)) ;
                                         set_file_value.set(c.masterjsonfile) ;
                                         set_see_editor.set(true) ;
                                         set_see_html.set(false) ;
