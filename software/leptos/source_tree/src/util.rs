c002e3a7e794fe5a3ec72c3e87b29295e6520537
Laurent Carrie <laurent.carrie@gmail.com>
2025-02-27 14:25:53 +0100

    x

--- software/leptos/source_tree/src/util.rs
+++ software/leptos/source_tree/src/util.rs
@@ -158,6 +158,7 @@ pub async fn get_request(choice: request::Choice) -> Result<(String, String)> {
     match data {
         Ok(x) => match x.choice {
             EChoice::ItemFileData(data) => Ok(("omake.stdout".to_string(), data)),
+            EChoice::OmakeProgress(data) => Ok(("omake progress".to_string(), data)),
             _ => Ok(("omake.stdout".to_string(), format!("{}:{}, bad type",file!(),line!()))),
         },
         Err(e) => {
