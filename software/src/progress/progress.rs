35a959aa14ea0739859d613ac64f7dc70699d72b
Laurent Carrie <laurent.carrie@gmail.com>
2025-02-27 12:12:18 +0100

    x

--- software/src/progress/progress.rs
+++ software/src/progress/progress.rs
@@ -1,4 +1,15 @@
-use crate::protocol::model::answer::Progress;
+use regex::Regex;
+use crate::protocol::model::answer::{Progress, ProgressItem};
+
 pub fn progress_of_string(data: String) -> Progress {
-    Progress { progress: vec![] }
+    let re = Regex::new(r"\[(.*)\]\[(.*)\]\[(.*)\]").unwrap();
+    let mut items: Vec<ProgressItem> = vec![];
+    for (_, [status, topic, message]) in re.captures_iter(&data).map(|c| c.extract()) {
+        items.push(ProgressItem {
+            status: status.to_string(),
+            topic: topic.to_string(),
+            message: message.to_string(),
+        });
+    };
+    Progress { progress: items }
 }
