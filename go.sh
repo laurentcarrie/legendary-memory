6cd8da4839bfcff957ca2320b7bdc99d474e2e5f
Laurent Carrie <laurent.carrie@gmail.com>
2025-04-02 16:32:07 +0200

    x

--- go.sh
+++ go.sh
@@ -5,7 +5,7 @@ set -x
 
 here=$(dirname $(realpath $0))
 
-#cargo install --path $here/software
+cargo install --path $here/software
 
 export songdir=$here/data/songs
 export bookdir=$here/data/books
@@ -13,13 +13,15 @@ export builddir=$here/build
 
 #export songdir=$here/data2/songs
 #export bookdir=$here/data2/books
-#export builddir=$here/build
+#xport builddir=$here/build
 
+rm -rf $here/data2/songs/*
+cp -R $here/data/songs/telephone $here/data2/songs/.
 
 rm -rf build/songs/telephone
 songbook $songdir $bookdir $builddir
 
 #( cd $builddir && omake -j 8 delivery/the_police--@--every_breath_you_take.pdf) || bash $builddir/omake.sh
 #( cd $builddir && omake -j 8 delivery/bashung--@--la_nuit_je_mens.pdf) || bash $builddir/omake.sh
-( cd $builddir && omake -j 8 delivery/telephone--@--au_coeur_de_la_nuit.pdf) || bash $builddir/omake.sh
-#( cd $builddir && omake -j 8 ) || bash $builddir/omake.sh
+#( cd $builddir && omake -j 8 delivery/telephone--@--au_coeur_de_la_nuit.pdf) || bash $builddir/omake.sh
+( cd $builddir && omake -j 8 ) || bash $builddir/omake.sh
